9fe314ca6d4ed66eb465c89dc10b88aecf137a5f
Nik <git@nik.lol>
2023-07-13 20:03:07 +0200

    chore: Add include/config.dist.h

---
+++ include/config.dist.h
@@ -0,0 +1,10 @@
+// To enable serial debug output
+//#define ESP_DEBUG
+
+#define WIFI_SSID "ssid"
+#define WIFI_PASS "wifi_password"
+
+#define IP_ADDRESS ""
+#define DNS ""
+#define GATEWAY ""
+#define SUBNET ""
