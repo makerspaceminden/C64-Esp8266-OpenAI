53fd0b1f7beec7abca785f674e8a0e403b1fdb24
Nik <git@nik.lol>
2023-07-13 20:46:52 +0200

    chore: Comment out static IP address definition

--- include/config.dist.h
+++ include/config.dist.h
@@ -4,7 +4,8 @@
 #define WIFI_SSID "ssid"
 #define WIFI_PASS "wifi_password"
 
-#define IP_ADDRESS ""
-#define DNS ""
-#define GATEWAY ""
-#define SUBNET ""
+// To enable static IP address uncomment and fill following constants
+// #define IP_ADDRESS ""
+// #define DNS ""
+// #define GATEWAY ""
+// #define SUBNET ""
